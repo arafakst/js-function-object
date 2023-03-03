73879b6875756b075aa62344ba456c11d121e742
arafakst <arafakst@gmail.com>
2023-03-04 01:51:21 +0600

    first commit

---
+++ function.js
@@ -0,0 +1,9 @@
+// Function declaration
+function startFan(){
+    console.log('Stand Up');
+    console.log('Walk toward the switch');
+    console.log('Press the switch');
+}
+
+// Call the function
+startFan();
