c39b3153bc4611dda9964902e03698caeb504814
arafakst <arafakst@gmail.com>
2023-03-04 19:08:23 +0600

    parameter

---
+++ parameter.js
@@ -0,0 +1,20 @@
+function bringSingara(money){
+
+    console.log('Ai nen singara');
+}
+
+//bringSingara(100);
+
+function add(num1, num2){
+    console.log('Add:', num1, num2);
+}
+
+// add(123, 160);
+
+function sum(a,b,c,d,e){
+    console.log(a,b,c,d,e);
+    var sum = a + b + c + d + e;
+    console.log(sum)
+}
+
+sum(12, 14, 16, 18, 20);
