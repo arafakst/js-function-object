d8a237106cd7b00110d7529e36fc18df85b50cbe
arafakst <arafakst@gmail.com>
2023-03-07 02:21:55 +0600

    switch

---
+++ switch.js
@@ -0,0 +1,43 @@
+const color = 'yellow';
+if(color === 'green'){
+    console.log('You are a green friend');
+}
+else if(color === 'blue'){
+    console.log('You are a blue friend');
+}
+else if(color === 'red'){
+    console.log('You are a red friend');
+}
+else if(color === 'white'){
+    console.log('You are a white friend');
+}
+else if(color === 'yellow'){
+    console.log('You are a yellow friend');
+}
+else{
+    console.log('you are a black black friend');
+}
+
+//Alternative of if else
+//Switch
+
+switch(color){
+    case 'green':
+        console.log('You are a green firned');
+        break;
+    case 'blue':
+        console.log('You are a blue firned');
+        break;
+    case 'red':
+        console.log('You are a red firned');
+        break;
+    case 'yellow':
+        console.log('You are a yellow firned');
+        break;
+    case 'white':
+            console.log('You are a white firend');
+            break;
+            default:
+                console.log('You are  a kala kala friend');
+
+}
